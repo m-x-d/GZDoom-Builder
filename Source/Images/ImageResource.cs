0c71d48d5fb47194ac805b1e79f6c7a7ead08781
codeimp <codeimp@e0d998f2-2e9b-42fe-843d-47128df60a06>
2007-09-24 17:41:36 +0000

    renamed/moved image resources/namespace and fixed file headers

--- Source/Images/ImageResource.cs
+++ Source/Images/ImageResource.cs
@@ -1,3 +1,21 @@
+
+#region ================== Copyright (c) 2007 Pascal vd Heiden
+
+/*
+ * Copyright (c) 2007 Pascal vd Heiden, www.codeimp.com
+ * This program is released under GNU General Public License
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ */
+
+#endregion
+
+#region ================== Namespaces
+
 using System;
 using System.Collections;
 using System.Collections.Generic;
@@ -7,7 +25,9 @@ using System.Drawing;
 using SlimDX.Direct3D9;
 using System.Drawing.Imaging;
 
-namespace CodeImp.DoomBuilder.Rendering
+#endregion
+
+namespace CodeImp.DoomBuilder.Images
 {
 	internal unsafe abstract class ImageResource : IDisposable
 	{
