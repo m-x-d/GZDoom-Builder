d89e443bf4e6fbbc7032b9ca06b2acf84feae84f
codeimp <codeimp@e0d998f2-2e9b-42fe-843d-47128df60a06>
2007-10-01 20:57:41 +0000

    drama

---
+++ trunk/Source/Data/DataManager.cs
@@ -0,0 +1,82 @@
+
+#region ================== Copyright (c) 2007 Pascal vd Heiden
+
+/*
+ * Copyright (c) 2007 Pascal vd Heiden, www.codeimp.com
+ * This program is released under GNU General Public License
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ */
+
+#endregion
+
+#region ================== Namespaces
+
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Text;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
+
+#endregion
+
+namespace CodeImp.DoomBuilder.Data
+{
+	internal class DataManager : IDisposable
+	{
+		#region ================== Constants
+
+		#endregion
+
+		#region ================== Variables
+
+		// Disposing
+		private bool isdisposed = false;
+
+		#endregion
+
+		#region ================== Properties
+
+		// Disposing
+		public bool IsDisposed { get { return isdisposed; } }
+
+		#endregion
+
+		#region ================== Constructor / Disposer
+
+		// Constructor
+		public DataManager()
+		{
+			// Initialize
+
+			// We have no destructor
+			GC.SuppressFinalize(this);
+		}
+
+		// Diposer
+		public void Dispose()
+		{
+			// Not already disposed?
+			if(!isdisposed)
+			{
+				// Clean up
+
+				// Done
+				isdisposed = true;
+			}
+		}
+
+		#endregion
+
+		#region ================== Methods
+
+		#endregion
+	}
+}
