6b2ba4f2d63abd5f33a5e8113c66ef25e93ec795
codeimp <codeimp@e0d998f2-2e9b-42fe-843d-47128df60a06>
2008-05-15 08:56:23 +0000

    added more missing files

---
+++ Source/BuilderModes/General/BuilderPlug.cs
@@ -0,0 +1,106 @@
+
+#region ================== Copyright (c) 2007 Pascal vd Heiden
+
+/*
+ * Copyright (c) 2007 Pascal vd Heiden, www.codeimp.com
+ * This program is released under GNU General Public License
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ */
+
+#endregion
+
+#region ================== Namespaces
+
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Text;
+using System.Windows.Forms;
+using System.IO;
+using System.Reflection;
+using CodeImp.DoomBuilder.Interface;
+using CodeImp.DoomBuilder.IO;
+using CodeImp.DoomBuilder.Map;
+using CodeImp.DoomBuilder.Rendering;
+using CodeImp.DoomBuilder.Geometry;
+using System.Drawing;
+using CodeImp.DoomBuilder.Editing;
+using CodeImp.DoomBuilder.Plugins;
+
+#endregion
+
+namespace CodeImp.DoomBuilder.BuilderModes
+{
+	public class BuilderPlug : Plug
+	{
+		#region ================== Constants
+
+		#endregion
+
+		#region ================== Variables
+
+		// Static instance
+		private static BuilderPlug me;
+		
+		// Main objects
+		private MenusForm menusform;
+
+		#endregion
+
+		#region ================== Properties
+
+		public static BuilderPlug Me { get { return me; } }
+
+		public MenusForm MenusForm { get { return menusform; } }
+
+		#endregion
+
+		#region ================== Initialize / Dispose
+
+		// When plugin is initialized
+		public override void OnInitialize()
+		{
+			// Setup
+			me = this;
+
+			// Load menus form and register it
+			menusform = new MenusForm();
+			menusform.Register();
+		}
+		
+		// Disposer
+		public override void Dispose()
+		{
+			// Not already disposed?
+			if(!IsDisposed)
+			{
+				// Clean up
+				menusform.Unregister();
+				menusform.Dispose();
+				
+				// Done
+				me = null;
+				base.Dispose();
+			}
+		}
+
+		#endregion
+
+		#region ================== Events
+
+		// When the editing mode changes
+		public override void OnModeChange(EditMode oldmode, EditMode newmode)
+		{
+			// Show the correct menu for the new mode
+			menusform.ShowEditingModeMenu(newmode);
+		}
+		
+		#endregion
+	}
+}
