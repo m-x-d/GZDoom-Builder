09a5e6bf78c48edd8d2dfc52b0b79dd24b287690
codeimp <codeimp@e0d998f2-2e9b-42fe-843d-47128df60a06>
2009-02-15 23:58:28 +0000

    Some more work done on TEXTURES support

--- trunk/Source/Data/TexturePatch.cs
+++ trunk/Source/Data/TexturePatch.cs
@@ -25,24 +25,63 @@ using System.Drawing;
 using System.Drawing.Imaging;
 using System.IO;
 using CodeImp.DoomBuilder.IO;
+using CodeImp.DoomBuilder.Rendering;
 
 #endregion
 
 namespace CodeImp.DoomBuilder.Data
 {
+	internal enum TexturePathRenderStyle
+	{
+		Copy,
+		Blend,
+		Add,
+		Subtract,
+		ReverseSubtract,
+		Modulate,
+		CopyAlpha
+	}
+	
 	internal struct TexturePatch
 	{
 		public string lumpname;
 		public int x;
 		public int y;
-
-		// Constructor
+		public bool flipx;
+		public bool flipy;
+		public int rotate;
+		public PixelColor blend;
+		public float alpha;
+		public TexturePathRenderStyle style;
+		
+		// Constructor for simple patches
 		public TexturePatch(string lumpname, int x, int y)
 		{
 			// Initialize
 			this.lumpname = lumpname;
 			this.x = x;
 			this.y = y;
+			this.flipx = false;
+			this.flipy = false;
+			this.rotate = 0;
+			this.blend = new PixelColor(0, 0, 0, 0);
+			this.alpha = 1.0f;
+			this.style = TexturePathRenderStyle.Copy;
+		}
+
+		// Constructor for hires patches
+		public TexturePatch(string lumpname, int x, int y, bool flipx, bool flipy, int rotate, PixelColor blend, float alpha, int style)
+		{
+			// Initialize
+			this.lumpname = lumpname;
+			this.x = x;
+			this.y = y;
+			this.flipx = flipx;
+			this.flipy = flipy;
+			this.rotate = rotate;
+			this.blend = blend;
+			this.alpha = alpha;
+			this.style = (TexturePathRenderStyle)style;
 		}
 	}
 }
