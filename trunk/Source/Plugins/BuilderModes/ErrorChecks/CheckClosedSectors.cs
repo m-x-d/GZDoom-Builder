0bc27fd0123bd2f7878040c08e3f0cabb8f27c5f
codeimp <codeimp@e0d998f2-2e9b-42fe-843d-47128df60a06>
2009-05-30 15:18:21 +0000

    Added better closed sectors error check (by Boris Iwanski)

--- trunk/Source/Plugins/BuilderModes/ErrorChecks/CheckClosedSectors.cs
+++ trunk/Source/Plugins/BuilderModes/ErrorChecks/CheckClosedSectors.cs
@@ -1,8 +1,8 @@
 
-#region ================== Copyright (c) 2007 Pascal vd Heiden
+#region ================== Copyright (c) 2009 Boris Iwanski
 
 /*
- * Copyright (c) 2007 Pascal vd Heiden, www.codeimp.com
+ * Copyright (c) 2009 Boris Iwanski
  * This program is released under GNU General Public License
  * 
  * This program is distributed in the hope that it will be useful,
@@ -67,88 +67,104 @@ namespace CodeImp.DoomBuilder.BuilderModes
 			int progress = 0;
 			int stepprogress = 0;
 			
+            // This is a simple yet effective way to check if a sector is closed.
+            // Each sidedef that belongs to a sector is checked out. The lines vertices
+            // are used as the key in a Dictionary. While going through all the
+            // sidedefs the values associated to the keys are set with a bitwise OR.
+            //
+            // The idea for this method was taken from the way DEU checks for closed
+            // sectors
+            //
+            // The starting vertex of the side gets its 1 bit set, the ending  vertex
+            // of the side gets its 2 bit set.
+            // The resulting dictionary will have 1, 2 or 3 as values (unlike the DEU
+            // version 0 will not appear here, since we only look at sides that belong
+            // to the current sector).
+            // Values 1 and 2 mean that a line they belong to has no side belonging to
+            // the current sector, thereby making the sector unclosed.
+            // A value of 3 usually means all lines belonging to that vertex have at least
+            // one side referencing the current sector. There are (rare) cases where this
+            // is not true, so a second check is done for all "opposite" vertices of the
+            // vertices with values 1 and 2: if the opposite vertex has a value of 3, but
+            // the line between the two vertices has no side referencing the current sector,
+            // then this falsely good vertex is added to the holes, too
+
 			// Go for all the sectors
 			foreach(Sector s in General.Map.Map.Sectors)
 			{
-				// Continue until all sidedefs have been traced
-				List<Vertex> foundholes = new List<Vertex>(2);
-				Dictionary<Sidedef, Sidedef> donesides = new Dictionary<Sidedef, Sidedef>(s.Sidedefs.Count);
-				foreach(Sidedef sd in s.Sidedefs)
-				{
-					if(!donesides.ContainsKey(sd))
-					{
-						// Trace the closest path starting at sd
-						Sidedef traceside = sd;
-						Vertex startvertex = (sd.IsFront) ? sd.Line.Start : sd.Line.End;
-						while(traceside != null)
-						{
-							// Mark this sidedef as done
-							donesides.Add(traceside, traceside);
-							
-							Vertex other = (startvertex == sd.Line.Start) ? sd.Line.End : sd.Line.Start;
-							List<Sidedef> nextsides = new List<Sidedef>(other.Linedefs.Count * 2);
-							bool foundsides = false;
-							foreach(Linedef l in other.Linedefs)
-							{
-								// Should we go along the front or back side?
-								if(l.Start == other)
-								{
-									// Front side of line connected to sector?
-									if((l.Front != null) && (l.Front.Sector == s))
-									{
-										// Add this sidedef
-										foundsides = true;
-										if(!donesides.ContainsKey(l.Front)) nextsides.Add(l.Front);
-									}
-								}
-								else
-								{
-									// Back side of line connected to sector?
-									if((l.Back != null) && (l.Back.Sector == s))
-									{
-										// Add this sidedef
-										foundsides = true;
-										if(!donesides.ContainsKey(l.Back)) nextsides.Add(l.Back);
-									}
-								}
-							}
-							
-							// Check if we can't go any further
-							if(!foundsides)
-							{
-								// This is where the sector is broken
-								foundholes.Add(other);
-								break;
-							}
-							else if(nextsides.Count > 1)
-							{
-								// This is done to ensure the tracing works along vertices that are shared by
-								// more than 2 lines/sides of the same sector. We must continue tracing along
-								// the first next smallest delta angle! This sorts the smallest delta angle to
-								// the top of the list.
-								SidedefAngleSorter sorter = new SidedefAngleSorter(traceside, other);
-								nextsides.Sort(sorter);
-							}
-							
-							if(nextsides.Count > 0)
-							{
-								// Set next sidedef to trace
-								traceside = nextsides[0];
-								startvertex = other;
-							}
-							else
-							{
-								// Nothing more to trace from here, leave
-								traceside = null;
-							}
-						}
-					}
-				}
-				
+                List<Vertex> foundholes = new List<Vertex>();
+                Dictionary<Vertex, int> vertices = new Dictionary<Vertex, int>();
+
+                // look at each side that belongs to the current sector
+                foreach (Sidedef sd in s.Sidedefs)
+                {
+                    // Add the lines starting vertex to the Dictionary if it's not yet present
+                    if (!vertices.ContainsKey(sd.Line.Start))
+                    {
+                        vertices.Add(sd.Line.Start, 0);
+                    }
+
+                    // Add the lines ending vertex to the Dictionary if it's not yet present
+                    if (!vertices.ContainsKey(sd.Line.End))
+                    {
+                        vertices.Add(sd.Line.End, 0);
+                    }
+
+                    // enable the 1 bit of the start vertex and the 2 bit of the end vertex of the side
+                    // This is from the sides point of view, where the side is always the "right" of line,
+                    // so start and end are flipped depending if the current side is the front of the
+                    // line or not
+                    if(sd.IsFront == true) {
+                        vertices[sd.Line.Start] |= 1;
+                        vertices[sd.Line.End] |= 2;
+                    }
+                    else
+                    {
+                        vertices[sd.Line.End] |= 1;
+                        vertices[sd.Line.Start] |= 2;
+                    }
+                }
+
+                // look at all the vertices
+                foreach (KeyValuePair<Vertex, int> kvp in vertices)
+                {
+                    // only look at bad vertices
+                    if (kvp.Value != 3)
+                    {
+                        // add the current vertex to the holes list
+                        if (!foundholes.Contains(kvp.Key))
+                        {
+                            foundholes.Add(kvp.Key);
+                        }
+
+                        // this checks if any vertices slipped past the first test
+                        // look at all lines that share the current vertex
+                        foreach (Linedef cl in kvp.Key.Linedefs)
+                        {
+                            // the line does not has its front or back side referencing the current sector
+                            if (!((cl.Front != null && cl.Front.Sector == s) || (cl.Back != null && cl.Back.Sector == s)))
+                            {
+                                // if the opposite vertex of our current vertex is marked good something must be wrong,
+                                // since the above check tells us that the line bewtween the vertices has no side referencing
+                                // the current sector. So add the falsely marked vertex to the holes list
+                                if (vertices.ContainsKey(cl.Start) && vertices[cl.Start] == 3 && !foundholes.Contains(cl.Start))
+                                {
+                                    foundholes.Add(cl.Start);
+                                }
+
+                                if (vertices.ContainsKey(cl.End) && vertices[cl.End] == 3 && !foundholes.Contains(cl.End))
+                                {
+                                    foundholes.Add(cl.End);
+                                }
+                            }
+                        }
+                    }
+                }
+
 				// Add report when holes have been found
 				if(foundholes.Count > 0)
 					SubmitResult(new ResultSectorUnclosed(s, foundholes));
-				
+
 				// Handle thread interruption
 				try { Thread.Sleep(0); }
 				catch(ThreadInterruptedException) { return; }
