9d0bd09f2561a4b54e5764b64185a5c4227774bb
codeimp <codeimp@e0d998f2-2e9b-42fe-843d-47128df60a06>
2009-07-07 20:59:56 +0000

    @ work in progress

---
+++ Source/Core/Windows/PasteOptionsForm.Designer.cs
@@ -0,0 +1,54 @@
+﻿namespace CodeImp.DoomBuilder.Windows
+{
+	partial class PasteOptionsForm
+	{
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if(disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.SuspendLayout();
+			// 
+			// PasteOptionsForm
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(96F, 96F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Dpi;
+			this.ClientSize = new System.Drawing.Size(454, 205);
+			this.ControlBox = false;
+			this.Font = new System.Drawing.Font("Arial", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+			this.MaximizeBox = false;
+			this.MinimizeBox = false;
+			this.Name = "PasteOptionsForm";
+			this.ShowIcon = false;
+			this.ShowInTaskbar = false;
+			this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
+			this.Text = "Paste Special";
+			this.ResumeLayout(false);
+
+		}
+
+		#endregion
+	}
+}
---
+++ Source/Core/Windows/PasteOptionsForm.cs
@@ -0,0 +1,58 @@
+﻿
+#region ================== Copyright (c) 2007 Pascal vd Heiden
+
+/*
+ * Copyright (c) 2007 Pascal vd Heiden, www.codeimp.com
+ * This program is released under GNU General Public License
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ */
+
+#endregion
+
+#region ================== Namespaces
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+using CodeImp.DoomBuilder.IO;
+using CodeImp.DoomBuilder.Map;
+using System.IO;
+using System.Collections;
+using System.Diagnostics;
+using CodeImp.DoomBuilder.Data;
+using CodeImp.DoomBuilder.Config;
+using CodeImp.DoomBuilder.Controls;
+
+#endregion
+
+namespace CodeImp.DoomBuilder.Windows
+{
+	public partial class PasteOptionsForm : DelayedForm
+	{
+		#region ================== Variables
+
+		#endregion
+
+		#region ================== Constructor
+
+		// Constructor
+		public PasteOptionsForm()
+		{
+			InitializeComponent();
+		}
+
+		#endregion
+
+		#region ================== Events
+
+		#endregion
+	}
+}
