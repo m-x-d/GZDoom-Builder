fae23686a24d04973ad0ef3513469fe9ee2228eb
codeimp <codeimp@e0d998f2-2e9b-42fe-843d-47128df60a06>
2009-07-17 15:06:15 +0000

    @ Added begin of DockersControl. This is going to be a long and tedious development.

---
+++ trunk/Source/Core/Controls/DockersControl.Designer.cs
@@ -0,0 +1,48 @@
+namespace CodeImp.DoomBuilder.Controls
+{
+	partial class DockersControl
+	{
+		/// <summary> 
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary> 
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if(disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Component Designer generated code
+
+		/// <summary> 
+		/// Required method for Designer support - do not modify 
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.SuspendLayout();
+			// 
+			// DockersControl
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(96F, 96F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Dpi;
+			this.BackColor = System.Drawing.SystemColors.Control;
+			this.Font = new System.Drawing.Font("Arial", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.Name = "DockersControl";
+			this.Size = new System.Drawing.Size(418, 364);
+			this.ResumeLayout(false);
+
+		}
+
+		#endregion
+
+	}
+}
