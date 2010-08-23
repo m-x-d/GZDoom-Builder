75ef865b086316fc9a8845c5eca4a540b2c24890
codeimp <codeimp@e0d998f2-2e9b-42fe-843d-47128df60a06>
2010-08-23 19:09:46 +0000

    @ remaining stuff

--- Source/Plugins/USDF/MainForm.Designer.cs
+++ Source/Plugins/USDF/MainForm.Designer.cs
@@ -28,14 +28,43 @@ namespace CodeImp.DoomBuilder.USDF
 		/// </summary>
 		private void InitializeComponent()
 		{
+			this.components = new System.ComponentModel.Container();
 			System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainForm));
+			this.tree = new System.Windows.Forms.TreeView();
+			this.treeimages = new System.Windows.Forms.ImageList(this.components);
 			this.SuspendLayout();
+			// 
+			// tree
+			// 
+			this.tree.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)
+						| System.Windows.Forms.AnchorStyles.Left)));
+			this.tree.HideSelection = false;
+			this.tree.ImageIndex = 0;
+			this.tree.ImageList = this.treeimages;
+			this.tree.Indent = 22;
+			this.tree.ItemHeight = 18;
+			this.tree.Location = new System.Drawing.Point(12, 12);
+			this.tree.Name = "tree";
+			this.tree.PathSeparator = ".";
+			this.tree.SelectedImageIndex = 0;
+			this.tree.Size = new System.Drawing.Size(257, 588);
+			this.tree.TabIndex = 0;
+			// 
+			// treeimages
+			// 
+			this.treeimages.ImageStream = ((System.Windows.Forms.ImageListStreamer)(resources.GetObject("treeimages.ImageStream")));
+			this.treeimages.TransparentColor = System.Drawing.Color.Transparent;
+			this.treeimages.Images.SetKeyName(0, "Dialog2.png");
+			this.treeimages.Images.SetKeyName(1, "book_closed.png");
+			this.treeimages.Images.SetKeyName(2, "book_open.png");
+			this.treeimages.Images.SetKeyName(3, "page_user.png");
 			// 
 			// MainForm
 			// 
 			this.AutoScaleDimensions = new System.Drawing.SizeF(96F, 96F);
 			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Dpi;
-			this.ClientSize = new System.Drawing.Size(813, 612);
+			this.ClientSize = new System.Drawing.Size(942, 612);
+			this.Controls.Add(this.tree);
 			this.Font = new System.Drawing.Font("Arial", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
 			this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
 			this.Name = "MainForm";
@@ -51,5 +80,9 @@ namespace CodeImp.DoomBuilder.USDF
 		}
 
 		#endregion
+
+		private System.Windows.Forms.TreeView tree;
+		private System.Windows.Forms.ImageList treeimages;
+
 	}
 }
