1a53b1886d917e29981e63e3f01e410747e78597
codeimp <codeimp@e0d998f2-2e9b-42fe-843d-47128df60a06>
2010-09-17 17:12:08 +0000

    @ Added missing file in previous commit

---
+++ trunk/Source/Plugins/GZDoomEditing/VisualModes/SectorLevelComparer.cs
@@ -0,0 +1,40 @@
+﻿#region === Copyright (c) 2010 Pascal van der Heiden ===
+
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Text;
+using CodeImp.DoomBuilder.Geometry;
+using CodeImp.DoomBuilder.Map;
+using CodeImp.DoomBuilder.Rendering;
+
+#endregion
+
+namespace CodeImp.DoomBuilder.GZDoomEditing
+{
+	internal class SectorLevelComparer : IComparer<SectorLevel>
+	{
+		// Center of sector to use for plane comparison
+		public Vector2D center;
+		
+		// Constructor
+		public SectorLevelComparer(Sector s)
+		{
+			this.center = new Vector2D(s.BBox.Left + s.BBox.Width / 2, s.BBox.Top + s.BBox.Height / 2);
+		}
+
+		// Comparer
+		public int Compare(SectorLevel x, SectorLevel y)
+		{
+			float delta = x.plane.GetZ(center) - y.plane.GetZ(center);
+
+			if(delta > 0.0f)
+				return 1;
+			else if(delta < 0.0f)
+				return -1;
+			else
+				return 0;
+		}
+	}
+}
