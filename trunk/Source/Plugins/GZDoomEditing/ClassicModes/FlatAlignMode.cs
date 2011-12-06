90d4ea11ff117bdf2af913c5a3b4646b52993dd4
codeimp <codeimp@e0d998f2-2e9b-42fe-843d-47128df60a06>
2011-12-06 15:35:11 +0000

    @ work in progress

---
+++ trunk/Source/Plugins/GZDoomEditing/ClassicModes/FlatAlignMode.cs
@@ -0,0 +1,115 @@
+﻿
+#region ================== Copyright (c) 2007 Pascal vd Heiden
+
+/*
+ * Copyright (c) 2007 Pascal vd Heiden, www.codeimp.com
+ * This program is released under GNU General Public License
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ */
+
+#endregion
+
+#region ================== Namespaces
+
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Text;
+using System.Windows.Forms;
+using System.IO;
+using System.Reflection;
+using CodeImp.DoomBuilder.Windows;
+using CodeImp.DoomBuilder.IO;
+using CodeImp.DoomBuilder.Map;
+using CodeImp.DoomBuilder.Rendering;
+using CodeImp.DoomBuilder.Geometry;
+using CodeImp.DoomBuilder.Editing;
+using System.Drawing;
+using CodeImp.DoomBuilder.Actions;
+
+#endregion
+
+namespace CodeImp.DoomBuilder.GZDoomEditing
+{
+	[EditMode(DisplayName = "Flat Align Mode",
+			  SwitchAction = "flatalignmode",
+			  ButtonImage = "VisualModeZ.png",
+			  ButtonOrder = int.MinValue + 210,
+			  ButtonGroup = "000_editing")]
+
+	public class FlatAlignMode : BaseClassicMode
+	{
+		#region ================== Constants
+
+		#endregion
+
+		#region ================== Variables
+
+		#endregion
+
+		#region ================== Constructor / Disposer
+
+		#endregion
+
+		#region ================== Methods
+
+		#endregion
+
+		#region ================== Events
+
+		// Mode engages
+		public override void OnEngage()
+		{
+			base.OnEngage();
+			renderer.SetPresentation(Presentation.Standard);
+			General.Map.Map.SelectionType = SelectionType.All;
+		}
+
+		// Mode disengages
+		public override void OnDisengage()
+		{
+			base.OnDisengage();
+
+			// Hide highlight info
+			General.Interface.HideInfo();
+		}
+
+		// This redraws the display
+		public override void OnRedrawDisplay()
+		{
+			renderer.RedrawSurface();
+
+			// Render lines
+			if(renderer.StartPlotter(true))
+			{
+				renderer.PlotLinedefSet(General.Map.Map.Linedefs);
+				renderer.PlotVerticesSet(General.Map.Map.Vertices);
+				renderer.Finish();
+			}
+
+			// Render things
+			if(renderer.StartThings(true))
+			{
+				renderer.RenderThingSet(General.Map.ThingsFilter.HiddenThings, Presentation.THINGS_HIDDEN_ALPHA);
+				renderer.RenderThingSet(General.Map.ThingsFilter.VisibleThings, 1.0f);
+				renderer.Finish();
+			}
+
+			// Render overlay
+			if(renderer.StartOverlay(true))
+			{
+				renderer.Finish();
+			}
+
+			renderer.Present();
+		}
+
+		#endregion
+	}
+}
