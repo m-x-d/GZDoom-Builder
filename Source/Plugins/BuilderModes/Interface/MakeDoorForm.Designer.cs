f5c8ce9466305a554996ff39258c81e8847a7c55
boris_i <boris_i@e0d998f2-2e9b-42fe-843d-47128df60a06>
2012-04-14 12:22:19 +0000

    Improved "make door" functionality. It now actually creates usable doors in Hexen and UDMF maps.

--- Source/Plugins/BuilderModes/Interface/MakeDoorForm.Designer.cs
+++ Source/Plugins/BuilderModes/Interface/MakeDoorForm.Designer.cs
@@ -36,11 +36,14 @@ namespace CodeImp.DoomBuilder.BuilderModes.Interface
 			this.label3 = new System.Windows.Forms.Label();
 			this.cancel = new System.Windows.Forms.Button();
 			this.apply = new System.Windows.Forms.Button();
+			this.resetoffsets = new System.Windows.Forms.CheckBox();
+			this.tracktexture = new CodeImp.DoomBuilder.Controls.TextureSelectorControl();
+			this.label4 = new System.Windows.Forms.Label();
 			this.SuspendLayout();
 			// 
 			// doortexture
 			// 
-			this.doortexture.Location = new System.Drawing.Point(22, 34);
+			this.doortexture.Location = new System.Drawing.Point(21, 34);
 			this.doortexture.Name = "doortexture";
 			this.doortexture.Required = false;
 			this.doortexture.Size = new System.Drawing.Size(96, 115);
@@ -49,7 +52,7 @@ namespace CodeImp.DoomBuilder.BuilderModes.Interface
 			// 
 			// label1
 			// 
-			this.label1.Location = new System.Drawing.Point(22, 15);
+			this.label1.Location = new System.Drawing.Point(21, 15);
 			this.label1.Name = "label1";
 			this.label1.Size = new System.Drawing.Size(96, 21);
 			this.label1.TabIndex = 1;
@@ -58,7 +61,7 @@ namespace CodeImp.DoomBuilder.BuilderModes.Interface
 			// 
 			// label2
 			// 
-			this.label2.Location = new System.Drawing.Point(132, 15);
+			this.label2.Location = new System.Drawing.Point(247, 15);
 			this.label2.Name = "label2";
 			this.label2.Size = new System.Drawing.Size(99, 21);
 			this.label2.TabIndex = 2;
@@ -67,7 +70,7 @@ namespace CodeImp.DoomBuilder.BuilderModes.Interface
 			// 
 			// ceilingtexture
 			// 
-			this.ceilingtexture.Location = new System.Drawing.Point(135, 34);
+			this.ceilingtexture.Location = new System.Drawing.Point(247, 34);
 			this.ceilingtexture.Name = "ceilingtexture";
 			this.ceilingtexture.Size = new System.Drawing.Size(96, 115);
 			this.ceilingtexture.TabIndex = 1;
@@ -75,7 +78,7 @@ namespace CodeImp.DoomBuilder.BuilderModes.Interface
 			// 
 			// floortexture
 			// 
-			this.floortexture.Location = new System.Drawing.Point(248, 34);
+			this.floortexture.Location = new System.Drawing.Point(360, 34);
 			this.floortexture.Name = "floortexture";
 			this.floortexture.Size = new System.Drawing.Size(96, 115);
 			this.floortexture.TabIndex = 2;
@@ -83,7 +86,7 @@ namespace CodeImp.DoomBuilder.BuilderModes.Interface
 			// 
 			// label3
 			// 
-			this.label3.Location = new System.Drawing.Point(245, 15);
+			this.label3.Location = new System.Drawing.Point(360, 15);
 			this.label3.Name = "label3";
 			this.label3.Size = new System.Drawing.Size(99, 21);
 			this.label3.TabIndex = 4;
@@ -94,7 +97,7 @@ namespace CodeImp.DoomBuilder.BuilderModes.Interface
 			// 
 			this.cancel.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
 			this.cancel.DialogResult = System.Windows.Forms.DialogResult.Cancel;
-			this.cancel.Location = new System.Drawing.Point(187, 180);
+			this.cancel.Location = new System.Drawing.Point(243, 180);
 			this.cancel.Name = "cancel";
 			this.cancel.Size = new System.Drawing.Size(112, 25);
 			this.cancel.TabIndex = 4;
@@ -105,7 +108,7 @@ namespace CodeImp.DoomBuilder.BuilderModes.Interface
 			// apply
 			// 
 			this.apply.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
-			this.apply.Location = new System.Drawing.Point(69, 180);
+			this.apply.Location = new System.Drawing.Point(125, 180);
 			this.apply.Name = "apply";
 			this.apply.Size = new System.Drawing.Size(112, 25);
 			this.apply.TabIndex = 3;
@@ -113,13 +116,46 @@ namespace CodeImp.DoomBuilder.BuilderModes.Interface
 			this.apply.UseVisualStyleBackColor = true;
 			this.apply.Click += new System.EventHandler(this.apply_Click);
 			// 
+			// resetoffsets
+			// 
+			this.resetoffsets.AutoSize = true;
+			this.resetoffsets.Checked = true;
+			this.resetoffsets.CheckState = System.Windows.Forms.CheckState.Checked;
+			this.resetoffsets.Location = new System.Drawing.Point(21, 156);
+			this.resetoffsets.Name = "resetoffsets";
+			this.resetoffsets.Size = new System.Drawing.Size(129, 18);
+			this.resetoffsets.TabIndex = 5;
+			this.resetoffsets.Text = "Reset texture offsets";
+			this.resetoffsets.UseVisualStyleBackColor = true;
+			// 
+			// tracktexture
+			// 
+			this.tracktexture.Location = new System.Drawing.Point(134, 34);
+			this.tracktexture.Name = "tracktexture";
+			this.tracktexture.Required = false;
+			this.tracktexture.Size = new System.Drawing.Size(96, 115);
+			this.tracktexture.TabIndex = 6;
+			this.tracktexture.TextureName = "";
+			// 
+			// label4
+			// 
+			this.label4.Location = new System.Drawing.Point(134, 15);
+			this.label4.Name = "label4";
+			this.label4.Size = new System.Drawing.Size(96, 21);
+			this.label4.TabIndex = 7;
+			this.label4.Text = "Track";
+			this.label4.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+			// 
 			// MakeDoorForm
 			// 
 			this.AcceptButton = this.apply;
 			this.AutoScaleDimensions = new System.Drawing.SizeF(96F, 96F);
 			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Dpi;
 			this.CancelButton = this.cancel;
-			this.ClientSize = new System.Drawing.Size(368, 219);
+			this.ClientSize = new System.Drawing.Size(481, 219);
+			this.Controls.Add(this.tracktexture);
+			this.Controls.Add(this.label4);
+			this.Controls.Add(this.resetoffsets);
 			this.Controls.Add(this.cancel);
 			this.Controls.Add(this.apply);
 			this.Controls.Add(this.floortexture);
@@ -139,6 +175,7 @@ namespace CodeImp.DoomBuilder.BuilderModes.Interface
 			this.Text = "Make Door";
 			this.HelpRequested += new System.Windows.Forms.HelpEventHandler(this.MakeDoorForm_HelpRequested);
 			this.ResumeLayout(false);
+			this.PerformLayout();
 
 		}
 
@@ -152,5 +189,8 @@ namespace CodeImp.DoomBuilder.BuilderModes.Interface
 		private System.Windows.Forms.Label label3;
 		private System.Windows.Forms.Button cancel;
 		private System.Windows.Forms.Button apply;
+		private System.Windows.Forms.CheckBox resetoffsets;
+		private CodeImp.DoomBuilder.Controls.TextureSelectorControl tracktexture;
+		private System.Windows.Forms.Label label4;
 	}
 }
