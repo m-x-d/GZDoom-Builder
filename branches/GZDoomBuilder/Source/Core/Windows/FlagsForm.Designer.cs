56c9bddc418e28d543577f2d2c58b932aa0aaac2
m-x-d <m-x-d@e0d998f2-2e9b-42fe-843d-47128df60a06>
2012-08-14 20:10:59 +0000

    Updated to Doom Builder 2 r1599: Find and Replace Mode: can now find things and linedefs by their flags

---
+++ branches/GZDoomBuilder/Source/Core/Windows/FlagsForm.Designer.cs
@@ -0,0 +1,113 @@
+﻿namespace CodeImp.DoomBuilder.Windows
+{
+	partial class FlagsForm
+	{
+		/// <summary>
+		/// Erforderliche Designervariable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Verwendete Ressourcen bereinigen.
+		/// </summary>
+		/// <param name="disposing">True, wenn verwaltete Ressourcen gelöscht werden sollen; andernfalls False.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Vom Windows Form-Designer generierter Code
+
+		/// <summary>
+		/// Erforderliche Methode für die Designerunterstützung.
+		/// Der Inhalt der Methode darf nicht mit dem Code-Editor geändert werden.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.flags = new CodeImp.DoomBuilder.Controls.CheckboxArrayControl();
+			this.cancel = new System.Windows.Forms.Button();
+			this.apply = new System.Windows.Forms.Button();
+			this.groupBox1 = new System.Windows.Forms.GroupBox();
+			this.groupBox1.SuspendLayout();
+			this.SuspendLayout();
+			// 
+			// flags
+			// 
+			this.flags.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)
+						| System.Windows.Forms.AnchorStyles.Left)
+						| System.Windows.Forms.AnchorStyles.Right)));
+			this.flags.AutoScroll = true;
+			this.flags.Columns = 3;
+			this.flags.Location = new System.Drawing.Point(6, 19);
+			this.flags.Name = "flags";
+			this.flags.Size = new System.Drawing.Size(418, 195);
+			this.flags.TabIndex = 0;
+			// 
+			// cancel
+			// 
+			this.cancel.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+			this.cancel.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+			this.cancel.Location = new System.Drawing.Point(351, 238);
+			this.cancel.Name = "cancel";
+			this.cancel.Size = new System.Drawing.Size(91, 25);
+			this.cancel.TabIndex = 4;
+			this.cancel.Text = "Cancel";
+			this.cancel.UseVisualStyleBackColor = true;
+			this.cancel.Click += new System.EventHandler(this.cancel_Click);
+			// 
+			// apply
+			// 
+			this.apply.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+			this.apply.Location = new System.Drawing.Point(254, 238);
+			this.apply.Name = "apply";
+			this.apply.Size = new System.Drawing.Size(91, 25);
+			this.apply.TabIndex = 3;
+			this.apply.Text = "OK";
+			this.apply.UseVisualStyleBackColor = true;
+			this.apply.Click += new System.EventHandler(this.apply_Click);
+			// 
+			// groupBox1
+			// 
+			this.groupBox1.Controls.Add(this.flags);
+			this.groupBox1.Location = new System.Drawing.Point(12, 7);
+			this.groupBox1.Name = "groupBox1";
+			this.groupBox1.Size = new System.Drawing.Size(430, 225);
+			this.groupBox1.TabIndex = 5;
+			this.groupBox1.TabStop = false;
+			// 
+			// FlagsForm
+			// 
+			this.AcceptButton = this.apply;
+			this.AutoScaleDimensions = new System.Drawing.SizeF(96F, 96F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Dpi;
+			this.CancelButton = this.cancel;
+			this.ClientSize = new System.Drawing.Size(454, 267);
+			this.Controls.Add(this.groupBox1);
+			this.Controls.Add(this.cancel);
+			this.Controls.Add(this.apply);
+			this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+			this.MaximizeBox = false;
+			this.MinimizeBox = false;
+			this.Name = "FlagsForm";
+			this.ShowIcon = false;
+			this.ShowInTaskbar = false;
+			this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
+			this.Text = "Flags";
+			this.Load += new System.EventHandler(this.LinedefFlagsForm_Load);
+			this.groupBox1.ResumeLayout(false);
+			this.ResumeLayout(false);
+
+		}
+
+		#endregion
+
+		private CodeImp.DoomBuilder.Controls.CheckboxArrayControl flags;
+		private System.Windows.Forms.Button cancel;
+		private System.Windows.Forms.Button apply;
+		private System.Windows.Forms.GroupBox groupBox1;
+	}
+}
