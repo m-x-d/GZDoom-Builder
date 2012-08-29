9642f9d4097ecae3e6b1bc0f83e9ccfa7b6bdfbf
m-x-d <m-x-d@e0d998f2-2e9b-42fe-843d-47128df60a06>
2012-08-29 10:09:02 +0000

    Added unknown things check to Error Checker

---
+++ branches/GZDoomBuilder/Source/Plugins/BuilderModes/ErrorChecks/ResultUnknownThing.cs
@@ -0,0 +1,41 @@
+﻿using System;
+//using System.Collections.Generic;
+//using System.Text;
+using CodeImp.DoomBuilder.Map;
+using CodeImp.DoomBuilder.Rendering;
+
+namespace CodeImp.DoomBuilder.BuilderModes.ErrorChecks {
+    public class ResultUnknownThing : ErrorResult {
+        public override int Buttons { get { return 1; } }
+        public override string Button1Text { get { return "Delete Thing"; } }
+
+        private Thing thing;
+
+        // Constructor
+        public ResultUnknownThing(Thing t) {
+			// Initialize
+			this.thing = t;
+			this.viewobjects.Add(t);
+			this.description = "This thing has unknown type (eg. it's not defined in DECORATE or current game configuration).";
+		}
+
+        // This must return the string that is displayed in the listbox
+		public override string ToString() {
+            return "Thing " + General.Map.Data.GetThingInfo(thing.Type).Index + " at " + thing.Position.x + ", " + thing.Position.y + " has unknown type.";
+		}
+
+		// Rendering
+		public override void  RenderOverlaySelection(IRenderer2D renderer) {
+			renderer.RenderThing(thing, renderer.DetermineThingColor(thing), 1.0f);
+		}
+		
+		// This removes the thing
+		public override bool Button1Click()	{
+			General.Map.UndoRedo.CreateUndo("Delete thing");
+			thing.Dispose();
+			General.Map.IsChanged = true;
+			General.Map.ThingsFilter.Update();
+			return true;
+		}
+    }
+}
