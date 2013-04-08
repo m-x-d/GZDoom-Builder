8f40e113fe586c7799dc8d8706c27777ac54b601
MaxED <j.maxed@gmail.com>
2013-04-08 14:37:37 +0000

    Added "Point Thing to cursor" action.

--- Source/Plugins/BuilderModes/Properties/Resources.Designer.cs
+++ Source/Plugins/BuilderModes/Properties/Resources.Designer.cs
@@ -214,6 +214,13 @@ namespace CodeImp.DoomBuilder.BuilderModes.Properties {
             }
         }
         
+        internal static System.Drawing.Bitmap ThingPointAtCursor {
+            get {
+                object obj = ResourceManager.GetObject("ThingPointAtCursor", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
         internal static System.Drawing.Bitmap treeview {
             get {
                 object obj = ResourceManager.GetObject("treeview", resourceCulture);
