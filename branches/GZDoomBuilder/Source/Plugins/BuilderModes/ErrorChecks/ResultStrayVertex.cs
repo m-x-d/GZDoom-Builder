e7869e97e5fb3b7651963095736a843a6bcc498b
m-x-d <m-x-d@e0d998f2-2e9b-42fe-843d-47128df60a06>
2013-05-30 15:50:46 +0000

    Map Analysis mode can now check the map for unconnected vertices.

---
+++ branches/GZDoomBuilder/Source/Plugins/BuilderModes/ErrorChecks/ResultStrayVertex.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using CodeImp.DoomBuilder.Map;
+using CodeImp.DoomBuilder.Rendering;
+using System.Drawing;
+
+namespace CodeImp.DoomBuilder.BuilderModes.ErrorChecks
+{
+	public class ResultStrayVertex : ErrorResult
+	{
+		public override int Buttons { get { return 1; } }
+		public override string Button1Text { get { return "Delete Vertex"; } }
+
+		private Vertex vertex;
+
+		// Constructor
+		public ResultStrayVertex(Vertex v) {
+			// Initialize
+			this.vertex = v;
+			this.viewobjects.Add(v);
+			this.description = "This vertex is not connected to any linedef.";
+		}
+
+		// This must return the string that is displayed in the listbox
+		public override string ToString() {
+			return "Vertex " + vertex.Index + " at " + vertex.Position.x + ", " + vertex.Position.y + " is not connected to any linedef.";
+		}
+
+		// Rendering
+		public override void RenderOverlaySelection(IRenderer2D renderer) {
+			renderer.RenderRectangleFilled(new RectangleF(vertex.Position.x - 3, vertex.Position.y - 3, 6f, 6f), General.Colors.Selection, true);
+		}
+
+		// This removes the vertex
+		public override bool Button1Click() {
+			General.Map.UndoRedo.CreateUndo("Delete vertex");
+			vertex.Dispose();
+			General.Map.IsChanged = true;
+			General.Map.ThingsFilter.Update();
+			return true;
+		}
+	}
+}
