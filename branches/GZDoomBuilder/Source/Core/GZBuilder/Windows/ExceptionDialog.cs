a95f6e2c03f32b4aa111c36a28be572db591af38
m-x-d <m-x-d@e0d998f2-2e9b-42fe-843d-47128df60a06>
2013-08-30 15:00:44 +0000

    Error Checking: renamed "Check unclosed sectors" to "Check invalid sectors". Now it also finds sectors with less than 3 sidedefs and sectors, which area is 0.

--- branches/GZDoomBuilder/Source/Core/GZBuilder/Windows/ExceptionDialog.cs
+++ branches/GZDoomBuilder/Source/Core/GZBuilder/Windows/ExceptionDialog.cs
@@ -38,6 +38,55 @@ namespace CodeImp.DoomBuilder.GZBuilder.Windows
 
 		public void Setup() {
 			bContinue.Enabled = !cannotContinue;
+
+			string[] titles = {
+				                  "0x000000 at 0xFFFFFF. That's probaby bad",
+                                  "Here we go again...",
+                                  "Uh oh, you're screwed",
+                                  "All is lost!",
+                                  "Achievement unlocked: CRASH TIME!",
+                                  "OH NOES! TEH ERROR!",
+                                  "0001000001111011000000000011001101011110110111",
+                                  "Nuclear launch detected!",
+                                  "Don't send this to Microsoft",
+                                  "You. Shall. Not. Pass!!!",
+                                  "Yep, we have bugs",
+                                  "It's dangerous to go alone. Take this!",
+                                  "The operation completed successfully",
+                                  "Security Alert – Moving cursor is not as safe as you thought",
+                                  "Random error appears from north",
+                                  "ERROR: NO_ERROR",
+                                  "Epic fail",
+                                  "At least it's not BSoD...",
+                                  "User Error. Please Replace User",
+                                  "Brought to you by MaxED!",
+                                  "GZDoom Builder proudly presents:",
+                                  "You aren't expected to understand this",
+                                  "Back to the drawing board...",
+                                  "I'm sorry... :(",
+                                  "This is a horrbble day for you, and of course, the world",
+                                  "Abort, Retry, Fail?",
+                                  "You are making progress. I'm afraid that's something I cannot allow to happen",
+                                  "You are making progress. That's not OK",
+								  "No errors found, restarting computer",
+                                  "Does Not Compute!",
+                                  "I’m sorry, Dave, I’m afraid I can’t do that",
+                                  "What's that? Chicken?",
+                                  "It can only be attributable to human error",
+                                  "It's now safe to turn off your computer",
+                                  "I've got a bad feeling about this",
+                                  "YOU CAN’T DO THAT!",
+                                  "Man the Lifeboats! Women and children first!",
+                                  "IMPOSSIBURU!!!",
+                                  "Now deleting all files. Goodbye",
+                                  "General Failure",
+                                  "Invalid Error",
+                                  "Beam me up Scotty, there’s no life out here",
+                                  "Well, you ran into something and the game is over",
+                                  "I'm good at writing bad code",                                  
+                                  "$FUNNY_ERROR_CAPTION",
+			                  };
+			this.Text = titles[new Random().Next(0, titles.Length - 1)];
 		}
 
 		private void reportLink_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e) {
