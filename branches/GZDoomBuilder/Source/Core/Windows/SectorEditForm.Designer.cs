b7f94af5153d61e848654c12c58e6243e10c0357
m-x-d <m-x-d@e0d998f2-2e9b-42fe-843d-47128df60a06>
2014-08-06 08:17:39 +0000

    Sector Edit Form: negative brightness can now be entered. Doom_misc.cfg, ZDoom_misc.cfg: changed the highest "sectorbrightness" value to 256 (was 255).

--- branches/GZDoomBuilder/Source/Core/Windows/SectorEditForm.Designer.cs
+++ branches/GZDoomBuilder/Source/Core/Windows/SectorEditForm.Designer.cs
@@ -175,7 +175,7 @@ namespace CodeImp.DoomBuilder.Windows
 			// brightness
 			// 
 			this.brightness.AllowDecimal = false;
-			this.brightness.AllowNegative = false;
+			this.brightness.AllowNegative = true;
 			this.brightness.AllowRelative = true;
 			this.brightness.ButtonStep = 8;
 			this.brightness.ButtonStepFloat = 1F;
