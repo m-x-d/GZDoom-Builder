ee8e31db37f909a5cee0599d5163596c0eacd5c3
MaxED <j.maxed@gmail.com>
2014-09-22 14:33:15 +0000

    Fixed: Visual mode: vavoom-type 3d floor sidedefs were not selectable. Fixed: Visual mode: "Select" action with "with the same textures" / "with the same height" modifiers (Shift/Ctrl + LMB) was not working properly when used on vavoom-type 3d floor sidedefs. Fixed: Visual mode, UDMF, "Auto-align textures" actions: vertical texture offset was calculated incorrectly for vavoom-type 3d floors.

--- Source/Plugins/BuilderModes/General/BuilderModesTools.cs
+++ Source/Plugins/BuilderModes/General/BuilderModesTools.cs
@@ -20,8 +20,18 @@ namespace CodeImp.DoomBuilder.BuilderModes
 				Rectangle rect = new Rectangle(0, 0, 1, 0);
 				Linedef cl = side.GetControlLinedef();
 				if(cl.Front != null && cl.Front.Sector != null) {
-					rect.Y = cl.Front.Sector.FloorHeight;
-					rect.Height = cl.Front.GetMiddleHeight();
+					// Use ceiling height for vavoom-type 3d floors. Also, FloorHeight is > CeilHeight for these...
+					if (cl.Args[1] == 0)
+					{
+						rect.Y = cl.Front.Sector.CeilHeight;
+						rect.Height = cl.Front.Sector.FloorHeight - cl.Front.Sector.CeilHeight;
+					}
+					else
+					{
+						rect.Y = cl.Front.Sector.FloorHeight;
+						rect.Height = cl.Front.GetMiddleHeight();
+					}
+					
 				} else {
 					rect.Y = side.Sidedef.Sector.FloorHeight;
 					rect.Height = side.Sidedef.GetMiddleHeight();
