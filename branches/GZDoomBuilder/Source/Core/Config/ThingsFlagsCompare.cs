5ecb44b76654941b0e0711c8b6fb061b3dc53f10
m-x-d <m-x-d@e0d998f2-2e9b-42fe-843d-47128df60a06>
2014-11-03 13:02:59 +0000

    Fixed, game configurations: Boom game configurations used incorrect "thingflagscompare" block. Fixed, Thing edit form: implemented overcomplicated required flags check (which should work as expected this time. probably.).

--- branches/GZDoomBuilder/Source/Core/Config/ThingsFlagsCompare.cs
+++ branches/GZDoomBuilder/Source/Core/Config/ThingsFlagsCompare.cs
@@ -17,6 +17,8 @@
 #region ================== Namespaces
 
 using System;
+using System.Collections.Generic;
+using System.Windows.Forms;
 using CodeImp.DoomBuilder.IO;
 using CodeImp.DoomBuilder.Map;
 
@@ -39,6 +41,10 @@ namespace CodeImp.DoomBuilder.Config
 		#region ================== Variables
 
 		private readonly string flag;
+		private string requiredgroup; //mxd. This flag only works if at least one flag is set in the "requiredgroup"
+		private string ignoredgroup; //mxd. If this flag is set, flags from ignoredgroup can be... well... ignored!
+		private string requiredflag; //mxd. This flag only works if requiredflag is set.
+		private readonly bool ingnorethisgroupwhenunset; //mxd
 		private readonly CompareMethod comparemethod;
 		private readonly bool invert;
 		private readonly string group;
@@ -49,6 +55,9 @@ namespace CodeImp.DoomBuilder.Config
 
 		public string Flag { get { return flag; } }
 		public string Group { get { return group; } }
+		public string RequiredGroup { get { return requiredgroup; } internal set { requiredgroup = value; } } //mxd
+		public string IgnoredGroup { get { return ignoredgroup; } internal set { ignoredgroup = value; } } //mxd
+		public string RequiredFlag { get { return requiredflag; } internal set { requiredflag = value; } } //mxd
 
 		#endregion
 
@@ -77,6 +86,10 @@ namespace CodeImp.DoomBuilder.Config
 			}
 
 			invert = cfg.ReadSetting(cfgpath + ".invert", false);
+			requiredgroup = cfg.ReadSetting(cfgpath + ".requiredgroup", string.Empty); //mxd
+			ignoredgroup = cfg.ReadSetting(cfgpath + ".ignoredgroup", string.Empty); //mxd
+			requiredflag = cfg.ReadSetting(cfgpath + ".requiredflag", string.Empty); //mxd
+			ingnorethisgroupwhenunset = cfg.ReadSetting(cfgpath + ".ingnorethisgroupwhenunset", false); //mxd
 			
 			// We have no destructor
 			GC.SuppressFinalize(this);
@@ -97,23 +110,153 @@ namespace CodeImp.DoomBuilder.Config
 			bool t2flag;
 
 			// Check if the flags exist
-			if(!t1.Flags.ContainsKey(flag) || !t2.Flags.ContainsKey(flag)) {
-				//mxd. If a map is in UDMF format - check Fields
-				if(!General.Map.UDMF || !t1.Fields.ContainsKey(flag) || !t2.Fields.ContainsKey(flag)) 
-					return 0;
+			if (!t1.Flags.ContainsKey(flag) || !t2.Flags.ContainsKey(flag)) return 0;
 
-				// tag flag inversion into account
-				t1flag = invert ? !(bool)t1.Fields[flag].Value : (bool)t1.Fields[flag].Value;
-				t2flag = invert ? !(bool)t2.Fields[flag].Value : (bool)t2.Fields[flag].Value;
-			} else {
-				// tag flag inversion into account
-				t1flag = invert ? !t1.Flags[flag] : t1.Flags[flag];
-				t2flag = invert ? !t2.Flags[flag] : t2.Flags[flag];
+			//mxd. We should ignore the flag if requiredgroup doesn't have any flags set
+			if(!string.IsNullOrEmpty(requiredgroup)) 
+			{
+				bool t1hasrequiredflags = false;
+				bool t2hasrequiredflags = false;
+				foreach(string key in General.Map.Config.ThingFlagsCompare[requiredgroup].Keys)
+				{
+					if(t1.Flags.ContainsKey(key) && (General.Map.Config.ThingFlagsCompare[requiredgroup][key].invert ? !t1.Flags[key] : t1.Flags[key])) 
+						t1hasrequiredflags = true;
+					if(t2.Flags.ContainsKey(key) && (General.Map.Config.ThingFlagsCompare[requiredgroup][key].invert ? !t2.Flags[key] : t2.Flags[key]))
+						t2hasrequiredflags = true;
+				}
+
+				// Can't compare...
+				if (!t1hasrequiredflags || !t2hasrequiredflags) return 0;
 			}
+
+			//mxd. We should ignore the flag if requiredflag is not set
+			if (string.IsNullOrEmpty(requiredflag))
+			{
+				bool inverted = General.Map.Config.ThingFlagsCompare[group].ContainsKey(requiredflag) && General.Map.Config.ThingFlagsCompare[group][requiredflag].invert;
+
+				bool t1hasrequiredflag = inverted ? !t1.Flags[requiredflag] : t1.Flags[requiredflag];
+				bool t2hasrequiredflag = inverted ? !t2.Flags[requiredflag] : t2.Flags[requiredflag];
+
+				// Can't compare...
+				if(!t1hasrequiredflag || !t2hasrequiredflag) return 0;
+			}
+
+			//mxd. We should also ignore the flag if it's in ingoredgroup
+			foreach(KeyValuePair<string, Dictionary<string, ThingFlagsCompare>> pair in General.Map.Config.ThingFlagsCompare)
+			{
+				foreach(KeyValuePair<string, ThingFlagsCompare> flaggrp in pair.Value)
+				{
+					if (!string.IsNullOrEmpty(flaggrp.Value.ignoredgroup) && group == flaggrp.Value.ignoredgroup)
+					{
+						bool t1ignoreflagset = flaggrp.Value.invert ? !t1.Flags[flaggrp.Key] : t1.Flags[flaggrp.Key];
+						bool t2ignoreflagset = flaggrp.Value.invert ? !t2.Flags[flaggrp.Key] : t2.Flags[flaggrp.Key];
+
+						// Can't compare...
+						if(!t1ignoreflagset || !t2ignoreflagset) return 0;
+					}	
+				}
+			}
+			
+			// Take flag inversion into account
+			t1flag = invert ? !t1.Flags[flag] : t1.Flags[flag];
+			t2flag = invert ? !t2.Flags[flag] : t2.Flags[flag];
 
 			if (comparemethod == CompareMethod.And && (t1flag && t2flag)) return 1;
 			if (comparemethod == CompareMethod.Equal && (t1flag == t2flag)) return 1;
 			return 0;
+		}
+
+		//mxd
+		public static string CheckThingEditFormFlags(List<CheckBox> checkboxes)
+		{
+			Dictionary<string, bool> flags = new Dictionary<string, bool>(checkboxes.Count);
+			Dictionary<string, Dictionary<string, bool>> flagspergroup = new Dictionary<string, Dictionary<string, bool>>(General.Map.Config.ThingFlagsCompare.Count);
+			Dictionary<string, bool> requiredgroups = new Dictionary<string, bool>();
+			Dictionary<string, bool> ignoredgroups = new Dictionary<string, bool>();
+
+			// Gather flags
+			foreach (CheckBox cb in checkboxes)
+			{
+				flags.Add(cb.Tag.ToString(), cb.CheckState == CheckState.Checked);
+			}
+
+			// Gather flags per group
+			foreach (KeyValuePair<string, Dictionary<string, ThingFlagsCompare>> group in General.Map.Config.ThingFlagsCompare)
+			{
+				flagspergroup.Add(group.Key, new Dictionary<string, bool>());
+
+				foreach (KeyValuePair<string, ThingFlagsCompare> flaggrp in group.Value)
+				{
+					bool flagset = IsFlagSet(flags, flaggrp.Key, flaggrp.Value.invert) && (string.IsNullOrEmpty(flaggrp.Value.requiredflag) || IsFlagSet(flags, flaggrp.Value.requiredflag, group.Value[flaggrp.Value.requiredflag].invert));
+
+					if(flagset)
+					{
+						flagspergroup[group.Key].Add(flaggrp.Key, true);
+
+						if(!string.IsNullOrEmpty(flaggrp.Value.requiredgroup) && !requiredgroups.ContainsKey(flaggrp.Value.requiredgroup))
+							requiredgroups.Add(flaggrp.Value.requiredgroup, false);
+					} 
+					else if(flaggrp.Value.ingnorethisgroupwhenunset)
+					{
+						ignoredgroups.Add(group.Key, false);
+					}
+				}
+			}
+
+			// Check dependancies
+			foreach (KeyValuePair<string, Dictionary<string, bool>> group in flagspergroup)
+			{
+				foreach(KeyValuePair<string, bool> flaggrp in group.Value)
+				{
+					if(!flaggrp.Value) continue;
+
+					string ignoredgrp = General.Map.Config.ThingFlagsCompare[group.Key][flaggrp.Key].ignoredgroup;
+					if (!string.IsNullOrEmpty(ignoredgrp) && !requiredgroups.ContainsKey(ignoredgrp))
+					{
+						ignoredgroups.Add(ignoredgrp, false);
+					}
+				}
+			}
+
+			// Get rid of ignoredgroups
+			foreach (KeyValuePair<string, bool> group in ignoredgroups)
+			{
+				flagspergroup.Remove(group.Key);
+			}
+
+			// Return message
+			string result = string.Empty;
+
+			foreach (KeyValuePair<string, Dictionary<string, bool>> group in flagspergroup)
+			{
+				if (group.Value.Count == 0)
+				{
+					switch(group.Key) 
+					{
+						case "skills":
+							result += "Thing is not used in any skill level.";
+							break;
+						case "gamemodes":
+							result += "Thing is not used in any game mode.";
+							break;
+						case "classes":
+							result += "Thing is not used by any class.";
+							break;
+						default:
+							result += "At least one '" + group.Key + "' flag should be set.";
+							break;
+					}
+				}
+			}
+
+			return result;
+		}
+
+		//mxd
+		private static bool IsFlagSet(Dictionary<string, bool> flags, string flag, bool invert)
+		{
+			bool result = flags.ContainsKey(flag) && flags[flag];
+			return (invert ? !result : result);
 		}
 
 		#endregion
