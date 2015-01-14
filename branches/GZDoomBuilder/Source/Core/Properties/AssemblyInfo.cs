9eddb34095e04784427fa01050739d64b8a0eb1f
m-x-d <m-x-d@e0d998f2-2e9b-42fe-843d-47128df60a06>
2015-01-14 13:12:23 +0000

    Draw Settings panel: added "Get From Selection" buttons. +a couple of cosmetic changes.

--- branches/GZDoomBuilder/Source/Core/Properties/AssemblyInfo.cs
+++ branches/GZDoomBuilder/Source/Core/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("GZDoom Builder")]
-[assembly: AssemblyDescription("GZDoom map editor")]
+[assembly: AssemblyDescription("ZDoom, GZDoom and Zandronum map editor")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("CodeImp, MaxED")]
 [assembly: AssemblyProduct("GZDoom Builder")]
-[assembly: AssemblyCopyright("Copyright © 2007, 2014")]
+[assembly: AssemblyCopyright("Copyright © 2007, 2015")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
