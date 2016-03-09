7bba7ae5f80868a480634cade7995e1a651821c8
m-x-d <m-x-d@e0d998f2-2e9b-42fe-843d-47128df60a06>
2016-03-09 14:52:18 +0000

    Added, Map Analysis mode: added "Check Polyobjects" checker. Changed, Game configurations: "Polyobject Anchor" things should not trigger "Thing outside the map geometry" error in the Map Analysis mode. Updated ZDoom_DECORATE.cfg (A_SetScale).

---
+++ branches/GZDoomBuilder/Source/Plugins/BuilderModes/ErrorChecks/ResultInvalidPolyobjectThings.cs
@@ -0,0 +1,89 @@
+﻿#region ================== Namespaces
+
+using System;
+using System.Collections.Generic;
+using CodeImp.DoomBuilder.Map;
+using CodeImp.DoomBuilder.Rendering;
+
+#endregion
+
+namespace CodeImp.DoomBuilder.BuilderModes
+{
+	public class ResultInvalidPolyobjectThings : ErrorResult
+	{
+		#region ================== Variables
+
+		private readonly List<Thing> things;
+		private readonly string thingsinfo;
+
+		#endregion
+
+		#region ================== Properties
+
+		public override int Buttons { get { return 0; } }
+
+		#endregion
+
+		#region ================== Constructor / Destructor
+
+		public ResultInvalidPolyobjectThings(List<Thing> things, string details) 
+		{
+			// Initialize
+			this.things = things;
+			this.hidden = true;
+			foreach(Thing t in things)
+			{
+				this.viewobjects.Add(t);
+				this.hidden &= t.IgnoredErrorChecks.Contains(this.GetType());
+			}
+
+			if(things.Count == 1)
+			{
+				thingsinfo = "Incorrect Polyobject setup for thing " + things[0].Index;
+			}
+			else
+			{
+				thingsinfo = "Incorrect Polyobject setup for things " + things[0].Index;
+				for(int i = 1; i < things.Count - 1; i++) thingsinfo += ", " + things[i].Index;
+				thingsinfo += " and " + things[things.Count - 1].Index;
+			}
+
+			this.description = thingsinfo + ": " + details;
+		}
+
+		#endregion
+
+		#region ================== Methods
+
+		// This sets if this result is displayed in ErrorCheckForm (mxd)
+		internal override void Hide(bool hide) 
+		{
+			hidden = hide;
+			Type t = this.GetType();
+			if(hide)
+			{
+				foreach(Thing thing in things) thing.IgnoredErrorChecks.Add(t);
+			}
+			else
+			{
+				foreach(Thing thing in things)
+					if(thing.IgnoredErrorChecks.Contains(t)) thing.IgnoredErrorChecks.Remove(t);
+			}
+		}
+
+		// This must return the string that is displayed in the listbox
+		public override string ToString()
+		{
+			return thingsinfo;
+		}
+
+		// Rendering
+		public override void RenderOverlaySelection(IRenderer2D renderer) 
+		{
+			foreach(Thing thing in things)
+				renderer.RenderThing(thing, General.Colors.Selection, Presentation.THINGS_ALPHA);
+		}
+
+		#endregion
+	}
+}
