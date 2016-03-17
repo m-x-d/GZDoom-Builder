476ccb5561ab5dfdb188b063547dbec74d9ddff3
MaxED <j.maxed@gmail.com>
2016-03-17 23:58:08 +0000

    Map saving: map lumps position in the wad is now kept persistent when saving it (previously / in DB2 map lumps were always pushed to the end of the wad when saving it). Internal: added NeutralResourcesLanguageAttribute to all assemblies. Theoretically this should result in a bit better performance when accessing embedded resources. Updated ZDoom_DECORATE.cfg (A_Chase flags).

--- Source/Plugins/BuilderModes/Properties/AssemblyInfo.cs
+++ Source/Plugins/BuilderModes/Properties/AssemblyInfo.cs
@@ -1,5 +1,6 @@
 ﻿using System.Reflection;
 using System.Runtime.InteropServices;
+using System.Resources;
 
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
@@ -29,3 +30,4 @@ using System.Runtime.InteropServices;
 //      Revision
 //
 [assembly: AssemblyVersion("2.3.0.2411")]
+[assembly: NeutralResourcesLanguageAttribute("en")]
