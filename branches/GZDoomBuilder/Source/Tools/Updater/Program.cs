cc58dc929e602ac089aafdcc942246252cfe044c
m-x-d <m-x-d@e0d998f2-2e9b-42fe-843d-47128df60a06>
2016-10-14 18:01:58 +0000

    Updater: moved SharpCompress code into a separate embedded library. Seems to stop some anti-viruses detecting it as a trojan/virus. Updated ZDoom_DECORATE.cfg (A_SetInventory).

--- branches/GZDoomBuilder/Source/Tools/Updater/Program.cs
+++ branches/GZDoomBuilder/Source/Tools/Updater/Program.cs
@@ -1,20 +1,29 @@
 using System;
+using System.Reflection;
 using System.Windows.Forms;
 
 namespace mxd.GZDBUpdater
 {
-    static class Program
-    {
-        /// <summary>
-        /// The main entry point for the application.
-        /// </summary>
-        [STAThread]
-        static void Main()
-        {
+	static class Program
+	{
+		/// <summary>
+		/// The main entry point for the application.
+		/// </summary>
+		[STAThread]
+		static void Main()
+		{
+			EmbeddedAssembly.Load("mxd.GZDBUpdater.SharpCompressStripped.dll", "SharpCompressStripped.dll");
+			AppDomain.CurrentDomain.AssemblyResolve += AssemblyResolve;
+			
 			Application.EnableVisualStyles();
-            Application.SetCompatibleTextRenderingDefault(false);
+			Application.SetCompatibleTextRenderingDefault(false);
 			MainForm form = new MainForm();
 			if(!form.IsDisposed) Application.Run(form);
-        }
-    }
+		}
+
+		private static Assembly AssemblyResolve(object sender, ResolveEventArgs args)
+		{
+			return EmbeddedAssembly.Get(args.Name);
+		}
+	}
 }
