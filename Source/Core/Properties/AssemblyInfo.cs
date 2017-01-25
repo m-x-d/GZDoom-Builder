5ebdbd12d552505216d71549799c444a9ccb3c8f
ZZYZX <zzyzx@virtual>
2017-01-25 17:31:03 +0200

    Fixed: 65:Plat_UpByValue had incorrect argument description

--- Source/Core/Properties/AssemblyInfo.cs
+++ Source/Core/Properties/AssemblyInfo.cs
@@ -30,6 +30,6 @@ using CodeImp.DoomBuilder;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion("2.3.0.2841")]
+[assembly: AssemblyVersion("2.3.0.2843")]
 [assembly: NeutralResourcesLanguageAttribute("en")]
-[assembly: AssemblyHash("8c9514f")]
+[assembly: AssemblyHash("ee93615")]
