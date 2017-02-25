1a8b9d404f993744169ef7c058fbbd8db0ca6542
ZZYZX <zzyzx@virtual>
2017-02-25 10:31:25 +0200

    Added: search&replace support for non-script arg0str

--- Source/Core/Properties/AssemblyInfo.cs
+++ Source/Core/Properties/AssemblyInfo.cs
@@ -30,6 +30,6 @@ using CodeImp.DoomBuilder;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion("2.3.0.2913")]
+[assembly: AssemblyVersion("2.3.0.2915")]
 [assembly: NeutralResourcesLanguageAttribute("en")]
-[assembly: AssemblyHash("5ff54ab")]
+[assembly: AssemblyHash("e7ed533")]
--- Source/Plugins/BuilderModes/Properties/AssemblyInfo.cs
+++ Source/Plugins/BuilderModes/Properties/AssemblyInfo.cs
@@ -29,5 +29,5 @@ using System.Resources;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion("2.3.0.2913")]
+[assembly: AssemblyVersion("2.3.0.2915")]
 [assembly: NeutralResourcesLanguageAttribute("en")]
